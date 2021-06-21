5af5ffb11fe4313d3a5b19c2e00dfd4eec39b819
smikeblog <smikemail@mail.ru>
2021-06-21 12:49:53 +0300

    changed colorscheme to iseberg

--- config.h
+++ config.h
@@ -105,27 +105,61 @@ float alpha = 0.8, alphaUnfocused = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+// ========================
+// below is modified Dracula theme
+// ========================
+
 	/* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
+//  [0] = "#282828", /* black   */
+//  [1] = "#f07178", /* red     */
+//  [2] = "#c3e88d", /* green   */
+//  [3] = "#ffcb6b", /* yellow  */
+//  [4] = "#82aaff", /* blue    */
+//  [5] = "#ff79c6", /* magenta */
+//  [6] = "#89ddff", /* cyan    */
+//  [7] = "#d0d0d0", /* white   */
+//
+//	/* 8 bright colors */
+//  [8]  = "#44475a", /* black   */
+//  [9]  = "#ff8b92", /* red     */
+//  [10] = "#50fa7b", /* green   */
+//  [11] = "#f1fa8c", /* yellow  */
+//  [12] = "#bd93f9", /* blue    */
+//  [13] = "#ff79c6", /* magenta */
+//  [14] = "#8be9fd", /* cyan    */
+//  [15] = "#ffffff", /* white   */
 
-	/* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
+// ========================
+// end of the iseberg theme
+// ========================
 
-//	[255] = 0,
+// ========================
+// below is the iseberg theme
+// ========================
+
+  /* 8 normal colors */
+  "#161821", /* black   */
+  "#e27878", /* red     */
+  "#b4be82", /* green   */
+  "#e2a478", /* yellow  */
+  "#84a0c6", /* blue    */
+  "#a093c7", /* magenta */
+  "#89b8c2", /* cyan    */
+  "#c6c8d1", /* white   */
+
+  /* 8 bright colors */
+  "#6b7089", /* black   */
+  "#e98989", /* red     */
+  "#c0ca8e", /* green   */
+  "#e9b189", /* yellow  */
+  "#91acd1", /* blue    */
+  "#ada0d3", /* magenta */
+  "#95c4ce", /* cyan    */
+  "#d2d4de", /* white   */
+
+// ========================
+// end of the iseberg theme
+// ========================
 
 	/* more colors can be added after 255 to use with DefaultXX */
   [256] = "#282a36", /* background */
